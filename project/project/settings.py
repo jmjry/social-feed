f26c937bf3d1434bc0ea51c9ef4dae81c59cd398
jmjry <jakeryan1998@icloud.com>
2022-02-18 20:12:45 +0000

    Hide secret key

--- project/project/settings.py
+++ project/project/settings.py
@@ -18,9 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e9tvgj%mofflm&oikfuu6koq2kp!o1dem2=*p)_$30m@_s4i22'
+# Import the secret key
+import env.py
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
