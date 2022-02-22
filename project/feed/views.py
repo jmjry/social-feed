877d576e6ebc80d0546faa988515229de9bd5f73
jmjry <jakeryan1998@icloud.com>
2022-02-22 21:10:29 +0000

    Added django template language to display post data

--- project/feed/views.py
+++ project/feed/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
 
+
+posts = [
+    {
+        'author': 'Jake',
+        'title': 'Post 1',
+        'content': 'This is the first post',
+        'date_posted': '22nd Feb 2022'
+    },
+
+    {
+        'author': 'Brad',
+        'title': 'Post 2',
+        'content': 'This is the second post',
+        'date_posted': '23rd Feb 2022'
+    }
+]
+
 def home(request):
-    return render(request, "feed/home.html")
+    context = {
+        'posts': posts,
+    }
+    return render(request, "feed/home.html", context)
