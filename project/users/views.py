158f6a67bbc53b415e95033c028473c5fd4be676
Jake Ryan <jakeryan1998@icloud.com>
2022-03-07 18:39:41 +0000

    Feedback after registering

--- project/users/views.py
+++ project/users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from turtle import home
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,6 +10,8 @@ def register(request):
        form = UserCreationForm(request.POST)
        if form.is_valid():
            username = form.cleaned_data.get("username")
+           messages.success(request,f'Account created for {username}')
+           return redirect("feed-home")
     else:  
         form = UserCreationForm()
     context = {"form": form}
