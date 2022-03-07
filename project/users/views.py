04f8999dca0185f01fdb1569e929128e033736a9
Jake Ryan <jakeryan1998@icloud.com>
2022-03-07 19:12:49 +0000

    Email field, feedback & registration page finished

--- project/users/views.py
+++ project/users/views.py
@@ -1,18 +1,26 @@
-from turtle import home
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 
+def set_form_fields(form):
+    form.fields["username"].help_text = ""
+    form.fields["password1"].help_text = ""
+    form.fields["password2"].label = "Confirm Password"
+    form.fields["password2"].help_text = ""
+
 def register(request):
     if request.method == "POST": 
-       form = UserCreationForm(request.POST)
+       form = UserRegisterForm(request.POST)
+       set_form_fields(form)
        if form.is_valid():
+           form.save()
            username = form.cleaned_data.get("username")
            messages.success(request,f'Account created for {username}')
            return redirect("feed-home")
     else:  
-        form = UserCreationForm()
+        form = UserRegisterForm()
+        set_form_fields(form)
     context = {"form": form}
     return render(request, "users/register.html", context)
