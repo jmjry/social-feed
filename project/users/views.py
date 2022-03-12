0311b4f8200b18df3c951dd56b04a31edbabdb93
Jake Ryan <jakeryan1998@icloud.com>
2022-03-12 14:02:41 +0000

    Registration, navigation bar and styles added

--- project/users/views.py
+++ project/users/views.py
@@ -4,22 +4,26 @@ from .forms import UserRegisterForm
 
 # Create your views here.
 
+
 def set_form_fields(form):
     form.fields["username"].help_text = ""
     form.fields["password1"].help_text = ""
     form.fields["password2"].label = "Confirm Password"
     form.fields["password2"].help_text = ""
 
+
 def register(request):
-    if request.method == "POST": 
-       form = UserRegisterForm(request.POST)
-       set_form_fields(form)
-       if form.is_valid():
-           form.save()
-           username = form.cleaned_data.get("username")
-           messages.success(request,f'Account created for {username}. You can now login.')
-           return redirect("login")
-    else:  
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        set_form_fields(form)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request, f"Account created for {username}. You can now login."
+            )
+            return redirect("login")
+    else:
         form = UserRegisterForm()
         set_form_fields(form)
     context = {"form": form}
