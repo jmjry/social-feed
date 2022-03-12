fe5d2559731006ff5fca75e757c4ba60a0516760
Jake Ryan <jakeryan1998@icloud.com>
2022-03-12 13:43:50 +0000

    Added login & logout with redirects

--- project/users/views.py
+++ project/users/views.py
@@ -17,8 +17,8 @@ def register(request):
        if form.is_valid():
            form.save()
            username = form.cleaned_data.get("username")
-           messages.success(request,f'Account created for {username}')
-           return redirect("feed-home")
+           messages.success(request,f'Account created for {username}. You can now login.')
+           return redirect("login")
     else:  
         form = UserRegisterForm()
         set_form_fields(form)
