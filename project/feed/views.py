bf1d3da5ce657d699310da50d4089ae7273f5d84
Jake Ryan <jakeryan1998@icloud.com>
2022-03-17 18:00:18 +0000

    Users can delete their posts

--- project/feed/views.py
+++ project/feed/views.py
@@ -1,6 +1,6 @@
 from .models import Post
 from django.contrib.auth.models import User
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class PostListView(ListView):
@@ -25,6 +25,15 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author: 
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author: 
