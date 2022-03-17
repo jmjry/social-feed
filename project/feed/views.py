c7b1fab01fadf0a575cf4322df72be2d151d26b6
Jake Ryan <jakeryan1998@icloud.com>
2022-03-17 16:57:16 +0000

    Can view details of each post

--- project/feed/views.py
+++ project/feed/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.models import User
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class PostListView(ListView):
     model = Post
     template_name = "feed/home.html"
     context_object_name = "posts"
     ordering = ["-date_posted"]
+
+class PostDetailView(DetailView):
+    model = Post
