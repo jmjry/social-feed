24877e1841c2730e85028519953e691b46029278
Jake Ryan <jakeryan1998@icloud.com>
2022-03-19 12:47:42 +0000

    Pagination for navigating blog posts added

--- project/feed/views.py
+++ project/feed/views.py
@@ -8,6 +8,7 @@ class PostListView(ListView):
     template_name = "feed/home.html"
     context_object_name = "posts"
     ordering = ["-date_posted"]
+    paginate_by = 2
 
 class PostDetailView(DetailView):
     model = Post
