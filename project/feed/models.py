00834a30e30cdd1107331ddb5ed480e5c202c7d3
Jake Ryan <jakeryan1998@icloud.com>
2022-03-23 19:07:52 +0000

    Updated content text max length

--- project/feed/models.py
+++ project/feed/models.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=2000)
+    content = models.TextField(max_length=4000)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
