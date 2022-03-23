37a8150e0990a18353a1344445173da83f7a0418
Jake Ryan <jakeryan1998@icloud.com>
2022-03-23 16:23:09 +0000

    Updated character limits on posts

--- project/feed/models.py
+++ project/feed/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=200)
+    content = models.TextField(max_length=2000)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -20,7 +20,7 @@ class Post(models.Model):
         })
         
 class Comment(models.Model):
-    content = models.TextField(max_length=1000000)
+    content = models.TextField(max_length=2000)
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
