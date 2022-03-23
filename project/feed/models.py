151d94f5bf442fefe54277bdadb6989936433cdf
Jake Ryan <jakeryan1998@icloud.com>
2022-03-23 08:32:41 +0000

    Added superuser & updated post length

--- project/feed/models.py
+++ project/feed/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
         })
         
 class Comment(models.Model):
-    content = models.TextField(max_length=200)
+    content = models.TextField(max_length=2000)
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
