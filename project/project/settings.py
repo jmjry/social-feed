d312c0262cfa15f3cd9d94f77a8ad708a8fd3e85
Jake Ryan <jakeryan1998@icloud.com>
2022-03-25 18:07:29 +0000

    Bug fix which produced multiple error messages

--- project/project/settings.py
+++ project/project/settings.py
@@ -17,7 +17,7 @@ from . import env
 SECRET_KEY = env.return_secret_key()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
