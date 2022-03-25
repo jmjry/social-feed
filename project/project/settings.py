e6a86fa7995162cc64c842b6cfaf91b91df09757
Jake Ryan <jakeryan1998@icloud.com>
2022-03-25 18:10:57 +0000

    Removed unused path

--- project/project/settings.py
+++ project/project/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
 # SECURITY WARNING: keep the secret key used in production secret!
 from . import env
