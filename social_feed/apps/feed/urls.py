7364c9100094118961c9e36498a05effd36cd648
jmjry <jakeryan1998@icloud.com>
2022-04-15 09:49:56 +0000

    Bug fixing static files

--- social_feed/apps/feed/urls.py
+++ social_feed/apps/feed/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import (
     PostListView,
     PostDetailView,
@@ -16,3 +18,6 @@ urlpatterns = [
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("feed/", feed, name="feed"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
